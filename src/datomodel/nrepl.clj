733725a6c2ce70706b5af3336922ae0bc1e5fb00
Dan Lentz <danlentz@gmail.com>
2015-09-13 12:07:19 -0400

    nrepl port

--- src/datomodel/nrepl.clj
+++ src/datomodel/nrepl.clj
@@ -1,7 +1,11 @@
 (ns datomodel.nrepl
   (:require [clojure.tools.nrepl.server :as nrepl-server]
+            [clojure.tools.logging      :as log]
+            [datomodel.edn              :as dedn]
             [cider.nrepl :refer [cider-nrepl-handler]]))
 
-(defn -main
-  []
-  (nrepl-server/start-server :port 7888 :handler cider-nrepl-handler))
+
+(defn -main []
+  (nrepl-server/start-server
+    :port (dedn/config-value :nrepl)
+    :handler cider-nrepl-handler))
