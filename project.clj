a645aac61253c1bc02c63206c7fcf3a8033a162c
Dan Lentz <danlentz@gmail.com>
2015-09-13 11:46:47 -0400

    deps: component, cider-nrepl

--- project.clj
+++ project.clj
@@ -4,18 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :resource-paths ["schema"]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time]]
+                 [com.stuartsierra/component "0.2.3"]
                  [datomic-schematode "0.1.0-RC3"]
-;                 [org.clojure/tools.nrepl "0.2.3"]
-                 [print-foo "0.5.3"]]
+                 [print-foo "1.0.2"]]
 
-  :resource-paths ["schema"]
-  :plugins [[lein-asciidoctor "0.1.14"]]
-  :asciidoc     {:sources ["doc/*.adoc"]
-                 :to-dir "doc/html"
-                 :toc              :left
-                 :doctype          :article
-                 :format           :html5
-                 :extract-css      true
-                 :source-highlight true})
+  :plugins [[lein-asciidoctor  "0.1.14"]
+            [cider/cider-nrepl "0.9.1"]]
+
+  :asciidoc {:sources ["doc/*.adoc"]
+             :to-dir "doc/html"
+             :toc              :left
+             :doctype          :article
+             :format           :html5
+             :extract-css      true
+             :source-highlight true})
